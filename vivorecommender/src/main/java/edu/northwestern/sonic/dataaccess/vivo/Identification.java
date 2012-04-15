bbab200c82cb63ab5422fe2024c45e7a3eb15a1c
Hugh Devlin <hdevlin@northwestern.edu>
2012-04-15 12:07:53 -0500

    refactor identification, add comments, remove throws

--- vivorecommender/src/main/java/edu/northwestern/sonic/dataaccess/vivo/Identification.java
+++ vivorecommender/src/main/java/edu/northwestern/sonic/dataaccess/vivo/Identification.java
@@ -1,31 +1,49 @@
 package edu.northwestern.sonic.dataaccess.vivo;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Set;
 
 public class Identification extends VivoSparqlService {
 
-	public Set<URI> identifyExpertsByResearchArea(String subject) throws URISyntaxException {
+	private String subjectFilter(String subject) {
+		StringBuffer returnValue = new StringBuffer("filter regex(?subject, \"");
+		returnValue.append(subject);
+		returnValue.append("\",\"i\")");
+		return returnValue.toString();
+	}
+
+	/**
+	 * @param subject a research area
+	 * @return set of URIs of qualified experts
+	 */
+	public Set<URI> identifyExpertsByResearchArea(String subject) {
 		StringBuffer query = new StringBuffer("?X vivo:hasResearchArea ?ResearchArea .\n");
 		query.append("?ResearchArea rdfs:label ?subject .\n");
-		query.append("filter regex(?subject, \"" + subject + "\",\"i\")");
+		query.append(subjectFilter(subject));
 		return getDistinctSortedURIs(query.toString());
 	}
 	
-	public Set<URI> identifyExpertsByKeyword(String subject) throws URISyntaxException {
+	/**
+	 * @param subject an article free text keyword
+	 * @return set of URIs of qualified experts
+	 */
+	public Set<URI> identifyExpertsByKeyword(String subject) {
 		StringBuffer query = new StringBuffer("?X vivo:authorInAuthorship ?pub .\n");
 		query.append("?pub vivo:linkedInformationResource ?keyword .\n");
 		query.append("?keyword vivo:freetextKeyword ?subject .\n");
-		query.append("filter regex(?subject, \"" + subject + "\",\"i\")");
+		query.append(subjectFilter(subject));
 		return getDistinctSortedURIs(query.toString());
 	}
 	
-	public Set<URI> identifyExpertsBySubjectArea(String subject) throws URISyntaxException {
+	/**
+	 * @param subject an article subject area
+	 * @return set of URIs of qualified experts
+	 */
+	public Set<URI> identifyExpertsBySubjectArea(String subject) {
 		StringBuffer query = new StringBuffer("?X vivo:authorInAuthorship ?pub .\n");
 		query.append("?pub vivo:hasSubjectArea ?area .\n");
 		query.append("?area rdfs:label ?subject .\n");
-		query.append("filter regex(?subject, \"" + subject + "\",\"i\")");
+		query.append(subjectFilter(subject));
 		return getDistinctSortedURIs(query.toString());
 	}
 	
