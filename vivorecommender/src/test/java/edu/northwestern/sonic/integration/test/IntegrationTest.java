f95723e5dda8a23e7c91971db08df6705c99fe55
Hugh Devlin <hdevlin@northwestern.edu>
2012-05-01 14:05:50 -0500

    fix to quit chromedriver after chrome tests

--- vivorecommender/src/test/java/edu/northwestern/sonic/integration/test/IntegrationTest.java
+++ vivorecommender/src/test/java/edu/northwestern/sonic/integration/test/IntegrationTest.java
@@ -5,6 +5,7 @@ import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
+import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
@@ -130,9 +131,18 @@ public abstract class IntegrationTest {
 		return webDriver;
 	}
 
+	/**
+	 * set attributes from statics
+	 */
+	@Before
+	public void before() {
+		setWebAppUrl(defaultWebAppUrl);
+		setWebDriver(webDriverFactory());
+	}
+
 	@After
 	public void after() {
-		getWebDriver().close();
+		getWebDriver().quit();
 	}
 
 }
